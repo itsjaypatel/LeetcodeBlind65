f241444da46530a91032452c7267804521629343
itsjaypatel <jaypatel-work@outlook.com>
2024-06-17 18:06:18 +0530

    added array/Problem5

--- src/array/Problem5.java
+++ src/array/Problem5.java
@@ -1,2 +1,30 @@
-package array;public class Problem5 {
+package array;
+
+public class Problem5 {
+
+    //    Link: https://leetcode.com/problems/maximum-subarray/description/
+
+    public static void main(String[] args) {
+
+        int ans = new Problem5().maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,});
+        System.out.println(ans);
+    }
+
+    public int maxSubArray(int[] nums) {
+
+        // Approch: Not to include subarray with negative sum
+        // TC - O(n)
+        // SC - O(1)
+
+        int maxSubArrSum = 0, curSubArrSum = 0;
+
+        for(int x : nums){
+            curSubArrSum += x;
+            maxSubArrSum = Math.max(maxSubArrSum, curSubArrSum);
+            if(curSubArrSum < 0){
+                curSubArrSum = 0;
+            }
+        }
+        return maxSubArrSum;
+    }
 }
